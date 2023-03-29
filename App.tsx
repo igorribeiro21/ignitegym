82af6cd5922f48377d9b3dc6fd5e62f3d0bccc42
igor.ribeiro <igorapalvesribeiro@gmail.com>
2023-03-29 20:01:08 -0300

    component-library - feat: started creating Signin screen

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/
 
 import { THEME } from './src/theme';
 import { Loading } from '@components/Loading';
+import { Signin } from '@screens/Signin';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -18,7 +19,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? < View /> : <Loading />}
+      {fontsLoaded ? < Signin /> : <Loading />}
     </NativeBaseProvider>
   );
 }
