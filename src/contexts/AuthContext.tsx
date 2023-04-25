b1da16617a67242838fd80147578543b6291655e
igor.ribeiro <igorapalvesribeiro@gmail.com>
2023-04-24 21:53:52 -0300

    backend - feat: used function in state with context

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,8 +1,9 @@
 import { UserDTO } from '@dtos/UserDTO';
-import { ReactNode, createContext } from 'react';
+import { ReactNode, createContext, useState } from 'react';
 
 export type AuthContextDataProps = {
     user: UserDTO;
+    signIn: (email: string, password: string) => void;
 }
 
 type AuthContextProviderProps = {
@@ -12,15 +13,23 @@ type AuthContextProviderProps = {
 export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps);
 
 export function AuthContextProvider({ children }: AuthContextProviderProps) {
+    const [user, setUser] = useState({
+        id: '1',
+        name: 'Igor Ribeiro',
+        email: 'teste@teste.com',
+        avatar: 'igor.png'
+    });
+
+    function signIn(email: string, password: string) {
+        setUser({
+            name: '',
+            email,
+            id: '',
+            avatar: ''
+        })
+    }
     return (
-        <AuthContext.Provider value={{
-            user: {
-                id: '1',
-                name: 'Igor',
-                email: 'teste@teste.com',
-                avatar: 'igor.png'
-            }
-        }}>
+        <AuthContext.Provider value={{ user, signIn }}>
             {children}
         </AuthContext.Provider>
     );
--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,20 +1,38 @@
 import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
 import { useNavigation } from '@react-navigation/native';
+import { Controller, useForm } from 'react-hook-form';
 
 import { AuthNavigatorRoutesProps } from '@routes/auth.routes';
+
+import { useAuth } from '@hooks/useAuth';
 
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
 import { Input } from '@components/Input';
 import { Button } from '@components/Button';
 
+
+type FormData = {
+    email: string;
+    password: string;
+}
+
 export function Signin() {
+    const { signIn } = useAuth();
 
     const navigation = useNavigation<AuthNavigatorRoutesProps>();
+
+    const { control, handleSubmit, formState: { errors } } = useForm<FormData>()
 
     function handleNewAccount() {
         navigation.navigate('signUp');
     }
+
+    function handleSignIn({ email, password }: FormData) {
+        signIn(email, password);
+    }
+
+
 
     return (
         <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
@@ -39,19 +57,36 @@ export function Signin() {
                         Acesse sua conta
                     </Heading>
 
-                    <Input
-                        placeholder="E-mail"
-                        keyboardType="email-address"
-                        autoCapitalize="none"
-                    />
-                    <Input
-                        placeholder="Senha"
-                        secureTextEntry
+                    <Controller
+                        control={control}
+                        name="email"
+                        rules={{ required: 'Informe o e-mail' }}
+                        render={({ field: { onChange } }) => (
+                            <Input
+                                placeholder="E-mail"
+                                keyboardType="email-address"
+                                autoCapitalize="none"
+                                onChangeText={onChange}
+                                errorMessage={errors.email?.message}
+                            />
+                        )}
                     />
 
-                    <Button
-                        title="Acessar"
+                    <Controller
+                        control={control}
+                        name="password"
+                        rules={{ required: 'Informe a senha' }}
+                        render={({ field: { onChange } }) => (
+                            <Input
+                                placeholder="Senha"
+                                secureTextEntry
+                                onChangeText={onChange}
+                                errorMessage={errors.password?.message}
+                            />
+                        )}
                     />
+
+                    <Button title="Acessar" onPress={handleSubmit(handleSignIn)} />
                 </Center>
 
                 <Center mt={24}>
